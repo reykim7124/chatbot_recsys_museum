4cc20e0c7a9907a0eeb4e2827e63f80e11877c22
reykim7124 <mrayhanhakim2@gmail.com>
2021-09-16 17:12:36 +0700

    add build and load data function

--- scripts/migrate.py
+++ scripts/migrate.py
@@ -1,5 +1,29 @@
-from typedb.client import TypeDB 
+from typedb.client import TypeDB, SessionType, TransactionType
 import os
+
+
+def build_museum_recsys_chatbot_graph(inputs):
+    with TypeDB.core_client("localhost:1729") as client:
+        with client.session("phone_calls", SessionType.DATA) as session:
+            for input in inputs:
+                print(
+                    "Loading from [" + input["data_path"] + "] into TypeDB ...")
+                load_data_into_typedb(input, session)
+
+
+def load_data_into_typedb(input, session):
+    items = parse_data_to_dictionaries(input)
+
+    for item in items:
+        with session.transaction(TransactionType.WRITE) as transaction:
+            typeql_insert_query = input["template"](item)
+            print("Executing TypeQL Query: " + typeql_insert_query)
+            transaction.query().insert(typeql_insert_query)
+            transaction.commit()
+
+    print("\nInserted " + str(len(items)) +
+          " items from [ " + input["data_path"] + "] into TypeDB.\n")
+
 
 dir_path = os.getcwd()
 # file path & functions for inserting data to knowledge graph
