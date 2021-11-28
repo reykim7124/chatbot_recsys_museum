fcf5afc5b8cbd5acbce9f7d5ba5b1bcde9491c54
reykim7124 <mrayhanhakim2@gmail.com>
2021-11-29 02:38:27 +0700

    test query return empty

--- scripts/typedb_scripts/coba_query.py
+++ scripts/typedb_scripts/coba_query.py
@@ -5,20 +5,23 @@ with TypeDB.core_client("localhost:1729") as client:
         options = TypeDBOptions.core()
         options.infer = True
         with session.transaction(TransactionType.READ, options) as read_transaction:
-            answer_iterator = read_transaction.query().match('match $m isa museum, has place-name $mpn, has facebook $fb;' + \
+            answer_iterator = read_transaction.query().match('match $m isa museum, has place-name $mpn;' + \
             '$sd1 isa schedule-day, has day "Senin";' + \
-            '$sd2 isa schedule-day, has day "Rabu";' + \
-            '$sd3 isa schedule-day, has day "Sabtu";' + \
+            '$sd2 isa schedule-day, has day "Selasa";' + \
+            # '$sd3 isa schedule-day, has day "Sabtu";' + \
             '(has-museum: $m, has-schedule-day: $sd1) isa schedule-days;' + \
             '(has-museum: $m, has-schedule-day: $sd2) isa schedule-days;' + \
-            '(has-museum: $m, has-schedule-day: $sd3) isa schedule-days;' + \
-            'get $mpn, $fb;' + \
+            '(has-museum: $m, $tt) isa ticket-types, has price $p;'  + \
+            '$p >= 0;'
+            '$p <= 100000;'
+            # '(has-museum: $m, has-schedule-day: $sd3) isa schedule-days;' + \
+            'get $mpn;' + \
             'offset 0;' + \
-            'limit 3;')
+            'limit 5;')
 
             for answer in answer_iterator:
-                # museum = answer
-                # print("Retrieved museum with name " + museum.get_value();
-                pprint(vars(answer))
+                museum = answer.get("mpn")
+                print("Retrieved museum with name " + museum.get_value())
+                # pprint(vars(answer))
                 # for ans in answer.map():
                 #     print(ans)
